c8732f124d2f6e4925057b71dee67d5691e99098
wolfieorama <wachirawamunyi@gmail.com>
2016-02-25 23:54:04 +0300

    added angular via bower and configuured assets in vendor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
+//= require angular/angular
+//= require angular-ui-router/release/angular-ui-router
 //= require turbolinks
-//= require_tree .
