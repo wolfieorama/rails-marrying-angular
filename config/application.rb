41acbfe50f7ba20ca5bf141765475eaf0837ee05
wolfieorama <wachirawamunyi@gmail.com>
2016-02-26 00:02:31 +0300

    attached moldule to app and added ctrller and factories for angular

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,10 @@ module RailsAngular
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    #for angular and bower 
+    #for angular and bower
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+    config.assets.paths << Rails.root.join('app', 'javascripts', 'controllers')
+    config.assets.paths << Rails.root.join('app', 'javascripts', 'factories')
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
